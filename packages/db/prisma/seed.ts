3b49db3635bab2660e520107567759cc5b055467
Mike Betzel <mbetzel@gmail.com>
2026-07-24 20:05:23 -0500

    fix:

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -6,6 +6,19 @@ async function main() {
   const alicePasswordHash = await bcrypt.hash('alice', 10);
   const bobPasswordHash = await bcrypt.hash('bob', 10);
   const charliePasswordHash = await bcrypt.hash('charlie', 10);
+  const adminPasswordHash = await bcrypt.hash('admin', 10);
+
+  await prisma.user.upsert({
+    where: { username: 'admin' },
+    update: { email: 'admin@boardgameti.me', role: 'ADMIN', passwordHash: adminPasswordHash },
+    create: {
+      username: 'admin',
+      email: 'admin@boardgameti.me',
+      role: 'ADMIN',
+      passwordHash: adminPasswordHash,
+      avatarUrl: 'https://api.dicebear.com/7.x/bottts/svg?seed=admin',
+    },
+  });
 
   const alice = await prisma.user.upsert({
     where: { username: 'alice' },
